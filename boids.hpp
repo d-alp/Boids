42b89ef0ab170f79e1508e6a58a86365bb53ce9e
Flavio Ballerini <balleriniflavio1906@gmail.com>
2023-02-28 15:47:44 +0100

    ciao

---
+++ boids.hpp
@@ -0,0 +1,14 @@
+class coordinates {
+  double x_;
+  double y_;
+
+ public:
+};
+
+class Boids {
+  coordinates position{};
+  coordinates velocity{};
+
+ public:
+  double distance(Boids const& b1, Boids const& b2);
+};
